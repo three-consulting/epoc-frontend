03ebd640b0640fef88a47b5f3e1e461dbfbad83e
robertsiipola <robert@three.consulting>
2021-11-11 22:32:32 +0200

    feat: added ErrorAlert to the projects folder for use with data fetching hooks

--- pages/projects/new.tsx
+++ pages/projects/new.tsx
@@ -5,16 +5,23 @@ import Layout from '@/components/common/Layout';
 import NewProjectForm from '@/components/projects/NewProject/NewProjectForm';
 import useCustomers from '@/lib/hooks/useCustomers';
 import useEmployees from '@/lib/hooks/useEmployees';
+import ErrorAlert from '@/components/common/ErrorAlert';
 
 const New: NextPage = () => {
-    const { customers } = useCustomers();
-    const { employees } = useEmployees();
+    const { customers, isError: customerError } = useCustomers();
+    const { employees, isError: employeeError } = useEmployees();
 
     return (
         <Layout>
             <Heading fontWeight="black" margin="1rem 0rem">
                 New project
             </Heading>
+            {(customerError || employeeError) && (
+                <ErrorAlert
+                    title="Error loading data"
+                    message="Could not load the required data from the server"
+                ></ErrorAlert>
+            )}
             <NewProjectForm customers={customers} employees={employees}></NewProjectForm>
         </Layout>
     );
