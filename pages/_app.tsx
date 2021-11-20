fc3b7ae6c3c28dc396cb711cbdea54c675bcc6d1
robertsiipola <robert@three.consulting>
2021-11-20 20:20:58 +0200

    feat: added hooks for auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
 import type { AppProps } from 'next/app';
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
+import Amplify from 'aws-amplify';
+import { AuthProvider } from '@/lib/contexts/AuthContext';
+import { setAmplify } from '@/lib/utils/setAmplify';
+
+const updatedAwsConfig = setAmplify();
+
+Amplify.configure(updatedAwsConfig);
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
-        <ChakraProvider>
-            <Component {...pageProps} />
-        </ChakraProvider>
+        <AuthProvider>
+            <ChakraProvider>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        </AuthProvider>
     );
 }
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Layout from '@/components/common/Layout';
-import { Box } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
+import { useSignIn, useSignout, useUser } from '@/lib/hooks/useAuth';
 
 const Home: NextPage = () => {
+    const user = useUser();
+    const signIn = useSignIn();
+    const signOut = useSignout();
+    console.log(user);
     return (
         <Layout>
             <Box>Nothing to see here</Box>
+            <Button colorScheme="teal" onClick={signIn}>
+                Login
+            </Button>
+            <Button colorScheme="orange" onClick={signOut}>
+                Logout
+            </Button>
         </Layout>
     );
 };
