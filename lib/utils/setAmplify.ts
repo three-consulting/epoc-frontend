01def1f962913545988e94c696c290f01a486254
robertsiipola <robert@three.consulting>
2021-11-20 20:20:58 +0200

    feat: added hooks for auth

---
+++ lib/utils/setAmplify.ts
@@ -0,0 +1,21 @@
+import config from '../../aws-exports';
+import { UpdatedAwsConfig } from '@/lib/types/auth';
+
+export function setAmplify(): UpdatedAwsConfig {
+    const isLocalhost = process.env.NODE_ENV === 'development' ? true : false;
+
+    const [localRedirectSignIn, productionRedirectSignIn] = config.oauth.redirectSignIn.split(',');
+
+    const [localRedirectSignOut, productionRedirectSignOut] = config.oauth.redirectSignOut.split(',');
+
+    const updatedAwsConfig = {
+        ...config,
+        oauth: {
+            ...config.oauth,
+            redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+            redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
+        },
+    };
+
+    return updatedAwsConfig;
+}
