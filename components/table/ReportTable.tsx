c37ddd61ad82ae17a5f8df56319fb8c27e07a182
Touko Haapanen <touko@three.consulting>
2022-06-03 15:24:35 +0300

    feat: display hours per customer

--- components/table/ReportTable.tsx
+++ components/table/ReportTable.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import { Flex } from "@chakra-ui/layout"
-import { TimesheetEntry } from "@/lib/types/apiTypes"
+import { Flex, ListItem, UnorderedList } from "@chakra-ui/layout"
+import { Customer, TimesheetEntry } from "@/lib/types/apiTypes"
 
 interface TotalHoursProps {
     startDate: string
@@ -8,11 +8,20 @@ interface TotalHoursProps {
     totalQuantity: number
 }
 
+interface CustomerHoursRowProps {
+    entries: TimesheetEntry[]
+    customer: Customer
+}
+
 interface ReportTableProps {
     entries: TimesheetEntry[]
+    customers: Customer[]
     startDate: string
     endDate: string
 }
+
+const entriesQuantitySum = (entries: TimesheetEntry[]) =>
+    entries.reduce((total, currentItem) => total + currentItem.quantity, 0)
 
 function TotalHours({
     startDate,
@@ -31,16 +40,30 @@ function TotalHours({
     )
 }
 
+function CustomerHoursRow({
+    entries,
+    customer,
+}: CustomerHoursRowProps): JSX.Element {
+    return (
+        <ListItem>
+            Total hours for {customer.name}: {entriesQuantitySum(entries)}
+        </ListItem>
+    )
+}
+
 function ReportTable({
     entries,
+    customers,
     startDate,
     endDate,
 }: ReportTableProps): JSX.Element {
-    const totalQuantity = entries.reduce(
-        (total, currentItem) => total + currentItem.quantity,
-        0
-    )
-
+    const entriesByCustomer = (
+        entriesToFilter: TimesheetEntry[],
+        customerId: number
+    ) =>
+        entriesToFilter.filter(
+            (entry) => entry.timesheet.project.customer?.id === customerId
+        )
     return (
         <Flex
             flexDirection="column"
@@ -50,11 +73,27 @@ function ReportTable({
             borderRadius="0.2rem"
             padding="1rem 1rem"
         >
+            <b>Total Hours per customer: </b>
+            <UnorderedList>
+                {customers.map(
+                    (customer) =>
+                        customer.id && (
+                            <CustomerHoursRow
+                                key={`customer-hours-row-${customer.id}`}
+                                entries={entriesByCustomer(
+                                    entries,
+                                    customer.id
+                                )}
+                                customer={customer}
+                            />
+                        )
+                )}
+            </UnorderedList>
             <b>Total hours: </b>
             <TotalHours
                 startDate={startDate}
                 endDate={endDate}
-                totalQuantity={totalQuantity}
+                totalQuantity={entriesQuantitySum(entries)}
             />
         </Flex>
     )
