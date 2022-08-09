a95a28bada58426188de5c2970b4fb9f2767a366
Touko Haapanen <touko@three.consulting>
2022-08-09 13:18:55 +0300

    feat: type metadata is now autogenerated automatically for every DTO

--- script/generateTypeMetadata.js
+++ script/generateTypeMetadata.js
@@ -8,6 +8,11 @@ const rl = readline.createInterface({
     terminal: false,
 })
 
+const dataTransferObects = [
+    "CustomerDTO", "EmployeeDTO", "ProjectDTO",
+    "TimesheetDTO", "TaskDTO", "TimeCategoryDTO", "TimesheetEntryDTO"
+]
+
 const fieldToMetadata = (field, required) =>
     `${field}: {required: ${required.includes(field)}}`
 
@@ -21,14 +26,24 @@ const schemaToString = (type, schema) => {
     return `${fieldMetadata}`
 }
 
-rl.on("line", (data) => {
-    const schema = JSON.parse(data)
-    const types = schema.components.schemas
-    const type = "ProjectDTO"
-    const out = `
-  export type FieldMetadata = { required: boolean }
+function GenerateFields(DTO) {
+    rl.on("line", (data) => {
+        const schema = JSON.parse(data)
+        const types = schema.components.schemas
+        const type = DTO
+        const out = `
+    
+      ${schemaToString(type.replace("DTO", ""), types[type])}`
+        // eslint-disable-next-line no-console
+        console.log(out)
+    })
+}
 
-  ${schemaToString(type.replace("DTO", ""), types[type])}`
-    // eslint-disable-next-line no-console
-    console.log(out)
-})
+// eslint-disable-next-line no-console
+rl.on("line", () => console.log(`export type FieldMetadata = { required: boolean }`))
+
+const writeFields = () => {
+    dataTransferObects.map((data) => (GenerateFields(data)))
+}
+
+writeFields()
