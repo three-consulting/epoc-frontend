39c1bd819c99f48cad8925053a98054bbbb61530
ronikeuru <roni@three.consulting>
2023-02-16 19:57:57 +0200

    feat: archived view for customers and projects

--- components/table/CustomerTable.tsx
+++ components/table/CustomerTable.tsx
@@ -1,8 +1,8 @@
 import { Customer } from "@/lib/types/apiTypes"
 import { Box } from "@chakra-ui/layout"
-import { Tbody } from "@chakra-ui/react"
+import { FormControl, FormLabel, Switch, Tbody } from "@chakra-ui/react"
 import { Table, Td, Th, Thead, Tr } from "@chakra-ui/table"
-import React from "react"
+import React, { useState } from "react"
 import Link from "next/link"
 import FormSection from "../common/FormSection"
 import FormButtons from "../common/FormButtons"
@@ -29,39 +29,85 @@ interface CustomerTableProps {
 
 function CustomerTable({ customers }: CustomerTableProps): JSX.Element {
     const router = useRouter()
+
+    const activeCustomers = customers.filter((cst) => cst.enabled)
+    const archivedCustomers = customers.filter((cst) => !cst.enabled)
+
+    const [showArchived, setShowArchived] = useState<boolean>(false)
+
     return (
-        <FormSection
-            header={customers ? "All customers" : "No customers found"}
-        >
-            {customers.length ? (
-                <Table variant="simple">
-                    <Thead>
-                        <Tr>
-                            <Th>Name</Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        {customers.map((customer) => (
-                            <CustomerRow
-                                customer={customer}
-                                key={`${customer.id}`}
-                            />
-                        ))}
-                    </Tbody>
-                </Table>
-            ) : (
-                <Box borderWidth="1px" padding="1rem" margin="1rem">
-                    No customers have been added yet.
-                </Box>
+        <>
+            <FormSection
+                header={
+                    activeCustomers
+                        ? "Active customers"
+                        : "No active customers found"
+                }
+            >
+                {activeCustomers.length ? (
+                    <Table variant="simple">
+                        <Thead>
+                            <Tr>
+                                <Th>Name</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {activeCustomers.map((customer) => (
+                                <CustomerRow
+                                    customer={customer}
+                                    key={`${customer.id}`}
+                                />
+                            ))}
+                        </Tbody>
+                    </Table>
+                ) : (
+                    <Box borderWidth="1px" padding="1rem" margin="1rem">
+                        No customers have been added yet.
+                    </Box>
+                )}
+                <FormButtons>
+                    <StyledButton
+                        buttontype="add"
+                        name="customer"
+                        onClick={() => router.push("/customer/new")}
+                    />
+                </FormButtons>
+            </FormSection>
+            {archivedCustomers && archivedCustomers.length > 0 && (
+                <>
+                    {showArchived && (
+                        <FormSection header={"Archived customers"}>
+                            <Table variant="simple">
+                                <Thead>
+                                    <Tr>
+                                        <Th>Name</Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {archivedCustomers.map((customer) => (
+                                        <CustomerRow
+                                            customer={customer}
+                                            key={`${customer.id}`}
+                                        />
+                                    ))}
+                                </Tbody>
+                            </Table>
+                        </FormSection>
+                    )}
+                    <FormControl display="flex" alignItems="center">
+                        <FormLabel htmlFor="show-archived" mb="0">
+                            {"Show archived customers: "}
+                        </FormLabel>
+                        <Switch
+                            id="show-archived"
+                            onChange={() =>
+                                setShowArchived((shw) => (shw ? !shw : shw))
+                            }
+                        />
+                    </FormControl>
+                </>
             )}
-            <FormButtons>
-                <StyledButton
-                    buttontype="add"
-                    name="customer"
-                    onClick={() => router.push("/customer/new")}
-                />
-            </FormButtons>
-        </FormSection>
+        </>
     )
 }
 
--- components/table/ProjectTable.tsx
+++ components/table/ProjectTable.tsx
@@ -1,6 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import { Box } from "@chakra-ui/layout"
-import { Table, Thead, Tr, Td, Th, Tbody } from "@chakra-ui/react"
+import {
+    Table,
+    Thead,
+    Tr,
+    Td,
+    Th,
+    Tbody,
+    FormControl,
+    FormLabel,
+    Switch,
+} from "@chakra-ui/react"
 import Link from "next/link"
 import { Project } from "@/lib/types/apiTypes"
 import FormSection from "../common/FormSection"
@@ -29,36 +39,102 @@ interface ProjectTableProps {
 
 function ProjectTable({ projects }: ProjectTableProps): JSX.Element {
     const router = useRouter()
+
+    const activePropjects = projects.filter((prj) => prj.status === "ACTIVE")
+    const archivedProjects = projects.filter((prj) => prj.status === "ARCHIVED")
+
+    const [showArchived, setShowArchived] = useState<boolean>(false)
+
     return (
-        <FormSection header={projects ? "All projects" : "No projects found"}>
-            {projects && (
-                <Box>
-                    <Table variant="simple">
-                        <Thead>
-                            <Tr>
-                                <Th>Name</Th>
-                                <Th>Client</Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            {projects.map((project) => (
-                                <ProjectRow
-                                    project={project}
-                                    key={`${project.id}`}
+        <>
+            <FormSection
+                header={
+                    activePropjects
+                        ? "Active projects"
+                        : "No active projects found"
+                }
+            >
+                <>
+                    {activePropjects.length > 0 && (
+                        <Box>
+                            <Table variant="simple">
+                                <Thead>
+                                    <Tr>
+                                        <Th>Name</Th>
+                                        <Th>Client</Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {activePropjects.map((project) => (
+                                        <ProjectRow
+                                            project={project}
+                                            key={`${project.id}`}
+                                        />
+                                    ))}
+                                </Tbody>
+                            </Table>
+                            <FormButtons>
+                                <StyledButton
+                                    buttontype="add"
+                                    onClick={() => router.push("/project/new")}
+                                    name="project"
                                 />
-                            ))}
-                        </Tbody>
-                    </Table>
-                    <FormButtons>
-                        <StyledButton
-                            buttontype="add"
-                            onClick={() => router.push("/project/new")}
-                            name="project"
+                            </FormButtons>
+                        </Box>
+                    )}
+                </>
+            </FormSection>
+            {archivedProjects.length > 0 && (
+                <>
+                    {showArchived && (
+                        <FormSection
+                            header={
+                                archivedProjects
+                                    ? "Archived projects"
+                                    : "No archived projects found"
+                            }
+                        >
+                            {archivedProjects && (
+                                <Table variant="simple">
+                                    <Thead>
+                                        <Tr>
+                                            <Th>Name</Th>
+                                            <Th>Client</Th>
+                                        </Tr>
+                                    </Thead>
+                                    <Tbody>
+                                        {archivedProjects.map((project) => (
+                                            <ProjectRow
+                                                project={project}
+                                                key={`${project.id}`}
+                                            />
+                                        ))}
+                                    </Tbody>
+                                </Table>
+                            )}
+                        </FormSection>
+                    )}
+                    <FormControl
+                        display="flex"
+                        alignItems="center"
+                        paddingX="2rem"
+                        paddingY="1rem"
+                    >
+                        <FormLabel
+                            htmlFor="show-archived"
+                            mb="0"
+                            fontWeight="bold"
+                        >
+                            {"Show archived projects: "}
+                        </FormLabel>
+                        <Switch
+                            id="show-archived"
+                            onChange={() => setShowArchived((shw) => !shw)}
                         />
-                    </FormButtons>
-                </Box>
+                    </FormControl>
+                </>
             )}
-        </FormSection>
+        </>
     )
 }
 
